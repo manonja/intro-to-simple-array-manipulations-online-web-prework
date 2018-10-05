027fba400dfb6cc9e235f0b1e03b6c115a78ab8f
manon jacquin <manon.jacquin@gmail.com>
2018-10-05 18:33:05 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,21 @@ def pop_with_args(array)
   array.pop(2)
 end
 
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end 
+
+def using_insert(array, element)
+  array.insert(element, 4)
+end 
+
 
 
