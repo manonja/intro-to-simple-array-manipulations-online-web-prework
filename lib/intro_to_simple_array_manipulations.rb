5c48521f45aa8d88fbf9315d5f7b4d21889508e6
manon jacquin <manon.jacquin@gmail.com>
2018-10-05 18:35:10 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,8 +28,11 @@ def using_concat(array1, array2)
 end 
 
 def using_insert(array, element)
-  array.insert(element, 4)
+  array.insert(4, element)
 end 
 
+def using_uniq(array)
+  array.uniq()
+end 
 
 
