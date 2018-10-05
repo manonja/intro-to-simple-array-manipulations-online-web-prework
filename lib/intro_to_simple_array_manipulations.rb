6bc34d2e4f5f3f4c9f064888c6ac6570a7f99227
manon jacquin <manon.jacquin@gmail.com>
2018-10-05 18:37:37 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,6 +36,14 @@ def using_uniq(array)
 end 
 
 def using_flatten(array)
-  array.flatten
+  array.flatten()
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, int)
+  array.delete
 
 
